418e302a6d7234427da102542bb9a961a90c00b8
Harshitjayaswal <harshitjayaswal@gmail.com>
2016-04-01 12:06:15 -0400

    Update handy_run.m

--- handy_run.m
+++ handy_run.m
@@ -1,6 +1,6 @@
 % This m-file runs the "HANDY" = Human and Nature DYnamics model as
 % developed by Motesharrei:
-% S. Motesharrei et al. / Ecological Economics 101 (2014) 90–102
+% S. Motesharrei et al. / Ecological Economics 101 (2014) 90â€“102
 % Human and nature dynamics (HANDY): Modeling inequality and use of
 % resources in the collapse or sustainability of societies
 % Safa Motesharrei, Jorge Rivas, Eugenia Kalnay
@@ -36,51 +36,3 @@ w_o = 0;                % initial quanity of wealth
 y0 = [nc_o ne_o y_o w_o];       % vector of intial conditions
 tspan = [0 500];                % time span of simulation
 [T,Y] = ode45(@handy_dot,tspan,y0);
-
-%% POST-PROCESS INFORMATION
-for i = 1:15 %why do you have it from 1to 14?
-
-% Calculate a "Gross Domestic Product"
-exp.labor = 0.3;
-exp.capital = 0.05*(i-1); %0.5;
-exp.nature_cons = 1- exp.labor - exp.capital;
-
-N = length(T);
-labor = Y(:,1);
-capital = Y(:,4);
-nature = Y(:,3);
-nature_cons = delta*labor.*nature;
-nature_rate = diff(Y(:,4));
-GDP  = capital.^exp.capital .* labor.^exp.labor .* nature_cons.^exp.nature_cons;
-
-% Find time at maximum values of key states and calculations
-maxx.GDP(i) = find(GDP==max(GDP));
-maxx.wealth(i) = find(capital==max(capital));
-maxx.nature_cons(i) = find(nature_cons==min(nature_cons));
-maxx.xc(i) = find(labor==max(labor));
-if Y(N,2)>0
-    maxx.xe(i) = find(Y(:,2)==max(Y(:,2)));
-else
-    maxx.xe(i) = 0;
-end
-
-
-end % for i
-
-%% Plotting
-% plot(T,Y(:,1),'b',T,Y(:,2),'c',T,Y(:,3),'r',T,Y(:,4),'k')
-subplot(2,1,1)
-plot(T,Y(:,1)/max(Y(:,1)),'b',T,Y(:,2)/max(Y(:,2)),...
-    'c',T,Y(:,3)/max(Y(:,3)),'r',T,Y(:,4)/max(Y(:,4)),'k')
-legend('xc','xe','nature','wealth')
-xlabel('years')
-
-
-subplot(2,1,2)
-xx = [0:.05:.7];
-plot([0:.05:.7],maxx.xc./maxx.GDP,'b',...
-    [0:.05:.7],maxx.nature_cons./maxx.GDP,'r',...
-    [0:.05:.7],maxx.wealth./maxx.GDP,'k');
-xlabel('GDP exponent')
-ylabel('time of maximum X relative to maximum Y')
-legend('xc/GDP','wealth/GDP','nature cons/GDP')
